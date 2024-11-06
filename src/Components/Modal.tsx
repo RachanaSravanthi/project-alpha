19a83a5df2a8886bd498cbeddf253ed311f06554
Delbin George <imdelbingeorge@gmail.com>
2024-11-06 21:19:10 +0530

    style: some code commented

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -3,6 +3,7 @@
 import { motion } from "framer-motion";
 import { X, ChevronLeft, ChevronRight } from "lucide-react";
 import { useEffect, useCallback } from "react";
+import VimeoEmbed from "./VEM";
 
 interface ModalProps {
     project: {
@@ -95,7 +96,7 @@ export default function Modal({ project, onClose, onPrevious, onNext }: ModalPro
                     <div className="container mx-auto px-4 py-8">
                         <h2 className="text-3xl font-bold text-white mb-8">{project.title}</h2>
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
-                            {project.images.map((image, index) => (
+                            {/* {project.images.map((image, index) => (
                                 <div key={index} className="aspect-video bg-gray-800 relative">
                                     <img
                                         src={image}
@@ -124,11 +125,13 @@ export default function Modal({ project, onClose, onPrevious, onNext }: ModalPro
                                         </svg>
                                     </button>
                                 </div>
-                            ))}
+                            ))} */}
+                            <VimeoEmbed iframeLink={project.iframeLink} />
                         </div>
                         <p className="text-white text-lg mb-12">{project.description}</p>
+                        {/*
                         <h3 className="text-2xl font-bold text-white mb-6">Top picks</h3>
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                             {[1, 2, 3].map((_, index) => (
                                 <div key={index} className="aspect-video bg-gray-800 relative">
                                     <img
@@ -142,7 +145,7 @@ export default function Modal({ project, onClose, onPrevious, onNext }: ModalPro
                                     </div>
                                 </div>
                             ))}
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </motion.div>
