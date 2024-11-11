e9bebb9b9c584aca656d892e68974359e70099cb
Delbin George <imdelbingeorge@gmail.com>
2024-11-11 20:12:34 +0530

    fix: rename pages to lowercase

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./Pages/ErrorPage.tsx";
-import AboutPage from "./Pages/AboutUs.tsx";
-import HomePage from "./Pages/HomePage.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import AboutPage from "./pages/AboutUs.tsx";
+import HomePage from "./pages/HomePage.tsx";
 import AdminUploadPage from "./components/AdminUploadPage.tsx";
-import AdminSignin from "./Components/AdminSignin.tsx";
-import PrivateRoute from "./Components/PrivateRoute.tsx";
+import AdminSignin from "./components/AdminSignin.tsx";
+import PrivateRoute from "./components/PrivateRoute.tsx";
 
 
 const appRouter = createBrowserRouter([
